a641e58e0b586dd7d7bd4f349e85a90ebd829472
katspaugh <ivan@mycs.com>
2017-02-26 23:21:58 +0100

    add jisho.org link

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,7 +7,7 @@ export default class Header extends React.Component {
         <h1>Insert a Japanese text:</h1>
 
         <div className="gloss__credits">
-          <span>Powered by <a href="http://www.atilika.org/">Kuromoji</a></span>
+          <span>Powered by <a href="http://www.atilika.org/">Kuromoji</a> and <a href="http://jisho.org/">Jisho.org</a></span>
 
           <a className="github-button" href="https://github.com/katspaugh/kuromoji-gloss" data-icon="octicon-star" data-style="mega" data-count-href="/katspaugh/kuromoji-gloss" data-count-api="/repos/katspaugh/kuromoji-gloss#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star katspaugh/kuromoji-gloss on GitHub">Star</a>
         </div>
